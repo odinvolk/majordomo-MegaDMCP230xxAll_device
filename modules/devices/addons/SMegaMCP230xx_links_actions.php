8c19cebadf53cf8abb614c0a2717c6fa497f9a51
Паша <odinwolk@gmail.com>
2020-05-25 14:51:31 +0300

    Create SMegaMCP230xx_links_actions.php

---
+++ modules/devices/addons/SMegaMCP230xx_links_actions.php
@@ -0,0 +1,66 @@
+<?php
+
+if ($link_type=='sensor_pass_port0') {
+   $p0 = (int)gg($device1['LINKED_OBJECT'].'.p0');
+   sg($object.'.status',$p0);
+}
+if ($link_type=='sensor_pass_port1') {
+   $p1 = (int)gg($device1['LINKED_OBJECT'].'.p1');
+   sg($object.'.status',$p1);
+}
+if ($link_type=='sensor_pass_port2') {
+   $p2 = (int)gg($device1['LINKED_OBJECT'].'.p2');
+   sg($object.'.status',$p2);
+}
+if ($link_type=='sensor_pass_port3') {
+   $p3 = (int)gg($device1['LINKED_OBJECT'].'.p3');
+   sg($object.'.status',$p3);
+}
+if ($link_type=='sensor_pass_port4') {
+   $p4 = (int)gg($device1['LINKED_OBJECT'].'.p4');
+   sg($object.'.status',$p4);
+}
+if ($link_type=='sensor_pass_port5') {
+   $p5 = (int)gg($device1['LINKED_OBJECT'].'.p5');
+   sg($object.'.status',$p5);
+}
+if ($link_type=='sensor_pass_port6') {
+   $p6 = (int)gg($device1['LINKED_OBJECT'].'.p6');
+   sg($object.'.status',$p6);
+}
+if ($link_type=='sensor_pass_port7') {
+   $p7 = (int)gg($device1['LINKED_OBJECT'].'.p7');
+   sg($object.'.status',$p7);
+}
+if ($link_type=='sensor_pass_port8') {
+   $p8 = (int)gg($device1['LINKED_OBJECT'].'.p8');
+   sg($object.'.status',$p8);
+}
+if ($link_type=='sensor_pass_port9') {
+   $p9 = (int)gg($device1['LINKED_OBJECT'].'.p9');
+   sg($object.'.status',$p9);
+}
+if ($link_type=='sensor_pass_port10') {
+   $p10 = (int)gg($device1['LINKED_OBJECT'].'.p10');
+   sg($object.'.status',$p10);
+}
+if ($link_type=='sensor_pass_port11') {
+   $p11 = (int)gg($device1['LINKED_OBJECT'].'.p11');
+   sg($object.'.status',$p11);
+}
+if ($link_type=='sensor_pass_port12') {
+   $p12 = (int)gg($device1['LINKED_OBJECT'].'.p12');
+   sg($object.'.status',$p12);
+}
+if ($link_type=='sensor_pass_port13') {
+   $p13 = (int)gg($device1['LINKED_OBJECT'].'.p13');
+   sg($object.'.status',$p13);
+}
+if ($link_type=='sensor_pass_port14') {
+   $p14 = (int)gg($device1['LINKED_OBJECT'].'.p14');
+   sg($object.'.status',$p14);
+}
+if ($link_type=='sensor_pass_port15') {
+   $p15 = (int)gg($device1['LINKED_OBJECT'].'.p15');
+   sg($object.'.status',$p15);
+}
