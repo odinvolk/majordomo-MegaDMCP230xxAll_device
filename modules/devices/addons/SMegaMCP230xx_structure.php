29ba7ff34889862fb7b6f2774e4fac9a5700c942
Паша <odinwolk@gmail.com>
2020-05-25 14:52:08 +0300

    Create SMegaMCP230xx_structure.php

---
+++ modules/devices/addons/SMegaMCP230xx_structure.php
@@ -0,0 +1,37 @@
+<?php
+ 
+$this->device_types['megadmcp230xx'] = array(
+        'TITLE'=>'Расширитель MegaDMCP230xx',
+        'PARENT_CLASS'=>'SDevices',
+        'CLASS'=>'SMegaDMCP230xx',
+        'METHODS'=>array(
+            'statusUpdated'=>array('DESCRIPTION'=>'Data Value updated event'),
+            'valueWorkUpdated'=>array('DESCRIPTION'=>'Work Value updated event'),
+            'refreshStats'=>array('DESCRIPTION'=>'Refreshes daily/monthly stats','_CONFIG_SHOW'=>1),
+        ),
+        'PROPERTIES'=>array(
+            'ipAddress'=>array('DESCRIPTION'=>'IP-адрес Меги','_CONFIG_TYPE'=>'num','_CONFIG_HELP'=>'SdDimmerMinMax'),
+            'Password'=>array('DESCRIPTION'=>'Пароль Меги','_CONFIG_TYPE'=>'num','_CONFIG_HELP'=>'SdDimmerMinMax'),
+            'PortSDA'=>array('DESCRIPTION'=>'Порт Меги SDA','_CONFIG_TYPE'=>'num','_CONFIG_HELP'=>'SdDimmerMinMax'),
+            'PortINT'=>array('DESCRIPTION'=>'Порт Меги INT','_CONFIG_TYPE'=>'num','_CONFIG_HELP'=>'SdDimmerMinMax'),
+            'Ext'=>array('DESCRIPTION'=>'Канал MCP230xx','_CONFIG_TYPE'=>'num','_CONFIG_HELP'=>'SdDimmerMinMax'),
+            'value'=>array('DESCRIPTION'=>'Data Value','ONCHANGE'=>'valueUpdated','DATA_KEY'=>1),
+            'stored_value'=>array('DESCRIPTION'=>'Month Value'),
+            'p0'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p1'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p2'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p3'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p4'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p5'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p6'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p7'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p8'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p9'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p10'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p11'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p12'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p13'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p14'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+            'p15'=>array('DESCRIPTION'=>'Канал MCP230xx'),
+        ),
+);
